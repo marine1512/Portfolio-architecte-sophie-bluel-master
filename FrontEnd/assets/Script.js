6c73432034f5577f26399223389fa07a957501e9
Marine Mignot <mignomarine@gmail.com>
2023-03-02 18:14:54 +0100

    Modif

--- FrontEnd/assets/Script.js
+++ FrontEnd/assets/Script.js
@@ -1,1 +1,9 @@
-const slide = [{},{},{},{}];
+fetch('http://localhost:5678/api/users/login')
+.then(res => console.log(res))
+
+
+let photo = document.getElementsByClassName('photo');
+photo = document.querySelector.src = "assets/images" + ".png"
+
+fetch('http://localhost:5678/api/works/1')
+.then(res => console.log(res))
